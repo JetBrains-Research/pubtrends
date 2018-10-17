0532f04aa617990ab415061ae043f5cb4ff10d52
Nicholas Kapralov <4dvlup@gmail.com>
2018-10-18 01:53:27 +0300

    Quick adaptation to log4j

---
+++ crawler/src/main/kotlin/PubmedCrawler.kt
@@ -0,0 +1,110 @@
+import org.apache.logging.log4j.LogManager
+import org.xml.sax.InputSource
+import org.xml.sax.SAXException
+import java.io.*
+import java.util.zip.GZIPInputStream
+import javax.xml.parsers.SAXParserFactory
+
+class PubmedCrawler {
+    private val logger = LogManager.getLogger(PubmedCrawler::class)
+    private val client = PubmedFTPClient()
+
+    fun downloadBaseline(limit : Int) {
+        var files = client.getBaselineXMLsList()
+        if (files != null) {
+            if (limit > 0) {
+                files = files.subList(0, limit)
+            }
+            logger.info("Found ${files.size} baseline file(s)")
+            for (file in files) {
+                logger.info("${file.name}: Downloading... ")
+                if (client.downloadBaselineFile(file.name, "data/")) {
+                    logger.info("${file.name}: Unpacking... ")
+                    if (unpack(file.name)) {
+                        logger.info("${file.name}: SUCCESS")
+                    } else {
+                        logger.info("${file.name}: FAILURE")
+                    }
+                } else {
+                    logger.info("${file.name}: FAILURE")
+                }
+            }
+        }
+    }
+
+    fun downloadUpdateFile(name : String) {
+        if (client.downloadUpdateFile(name, "data/")) {
+            logger.info("$name: SUCCESS")
+        } else {
+            logger.info("$name: FAILURE")
+        }
+    }
+
+    fun parse(name : String) {
+        val spf = SAXParserFactory.newInstance()
+        spf.isNamespaceAware = true
+
+        val saxParser = spf.newSAXParser()
+        val pubmedXMLHandler = PubmedXMLHandler()
+        val xmlReader = saxParser.xmlReader
+        xmlReader.contentHandler = pubmedXMLHandler
+        try {
+            xmlReader.parse(InputSource(File(name).inputStream()))
+        } catch (e: SAXException) {
+            e.printStackTrace()
+        }
+
+        for (tag in pubmedXMLHandler.tags.keys) {
+            println("$tag ${pubmedXMLHandler.tags[tag]}")
+        }
+    }
+
+    fun update() {
+        // TODO: to config
+        // Timestamp (ms) of 14th Oct 18 ~10:30 to download 1 new XML
+        val lastCheck : Long = 1539513468000
+
+        val files = client.getNewXMLsList(lastCheck)
+        if (files != null) {
+            logger.info("Found ${files.size} new file(s)")
+            for (file in files) {
+                logger.info("${file.name}: Downloading... ")
+                if (client.downloadUpdateFile(file.name, "data/")) {
+                    logger.info("${file.name}: Unpacking... ")
+                    if (unpack(file.name)) {
+                        logger.info("${file.name}: SUCCESS")
+                    } else {
+                        logger.info("${file.name}: FAILURE")
+                    }
+                } else {
+                    logger.info("${file.name}: FAILURE")
+                }
+            }
+        }
+    }
+
+    fun unpack(archiveName : String) : Boolean {
+        val archive = File("data/$archiveName")
+        val originalName = "data/${archiveName.substringBefore(".gz")}"
+        val bufferSize = 1024
+        var safeUnpack = true
+
+        GZIPInputStream(BufferedInputStream(FileInputStream("data/$archiveName"))).use { inputStream ->
+            BufferedOutputStream(FileOutputStream(originalName)).use { outputStream ->
+                try {
+                    inputStream.copyTo(outputStream, bufferSize)
+                } catch (e : EOFException) {
+                    logger.warn("Corrupted GZ archive. ")
+                    e.printStackTrace()
+                    safeUnpack = false
+                }
+            }
+        }
+
+        if (safeUnpack) {
+            archive.delete()
+        }
+
+        return safeUnpack
+    }
+}
--- crawler/src/main/kotlin/main.kt
+++ crawler/src/main/kotlin/main.kt
@@ -1,108 +1,15 @@
+import org.xml.sax.EntityResolver
 import org.xml.sax.InputSource
+import org.xml.sax.SAXException
 import java.io.*
 import java.util.zip.GZIPInputStream
 import javax.xml.parsers.SAXParserFactory
 
-fun downloadBaseline(limit : Int) {
-    val client = PubmedFTPClient()
-    var files = client.getBaselineXMLsList()
-    if (files != null) {
-        if (limit > 0) {
-            files = files.subList(0, limit)
-        }
-        println("Found ${files.size} baseline file(s)")
-        for (file in files) {
-            print("${file.name}: Downloading... ")
-            if (client.downloadBaselineFile(file.name, "data/")) {
-                print("Unpacking... ")
-                if (unpack(file.name)) {
-                    println("SUCCESS")
-                } else {
-                    println("FAILURE")
-                }
-            } else {
-                println("${file.name}: FAILURE")
-            }
-        }
-    }
-}
-
-fun downloadUpdateFile(name : String) {
-    val client = PubmedFTPClient()
-    if (client.downloadUpdateFile(name, "data/")) {
-        println("$name: SUCCESS")
-    } else {
-        println("$name: FAILURE")
-    }
-}
-/*
-fun parse(name : String) {
-    val spf = SAXParserFactory.newInstance()
-    spf.isNamespaceAware = true
-
-    val saxParser = spf.newSAXParser()
-    val pubmedXMLHandler = PubmedXMLHandler()
-    val xmlReader = saxParser.xmlReader
-    xmlReader.contentHandler = pubmedXMLHandler
-    xmlReader.parse(InputSource(File(name).inputStream()))
-
-    for (tag in pubmedXMLHandler.tags.keys) {
-        println("$tag - ${pubmedXMLHandler.tags[tag]}")
-    }
-}
-*/
-fun update() {
-    // TODO: to config
-    // Timestamp (ms) of 14th Oct 18 ~10:30 to download 1 new XML
-    val lastCheck : Long = 1539513468000
-
-    val client = PubmedFTPClient()
-    val files = client.getNewXMLsList(lastCheck)
-    if (files != null) {
-        println("Found ${files.size} new file(s)")
-        for (file in files) {
-            print("${file.name}: Downloading... ")
-            if (client.downloadUpdateFile(file.name, "data/")) {
-                print("Unpacking... ")
-                if (unpack(file.name)) {
-                    println("SUCCESS")
-                } else {
-                    println("FAILURE")
-                }
-            } else {
-                println("${file.name}: FAILURE")
-            }
-        }
-    }
-}
-
-fun unpack(archiveName : String) : Boolean {
-    val archive = File("data/$archiveName")
-    val originalName = "data/${archiveName.substringBefore(".gz")}"
-    val bufferSize = 1024
-    var safeUnpack = true
-
-    GZIPInputStream(BufferedInputStream(FileInputStream("data/$archiveName"))).use { inputStream ->
-        BufferedOutputStream(FileOutputStream(originalName)).use { outputStream ->
-            try {
-                inputStream.copyTo(outputStream, bufferSize)
-            } catch (e : EOFException) {
-                print("Warning: Corrupted GZ archive. ")
-                e.printStackTrace()
-                safeUnpack = false
-            }
-        }
-    }
-
-    if (safeUnpack) {
-        archive.delete()
-    }
-
-    return safeUnpack
-}
-
 fun main(args: Array<String>) {
-//    downloadBaseline(10)
-//    parse("data/0001.xml")
-    update()
+//    TODO: CLI args processing
+
+    val crawler = PubmedCrawler()
+//    crawler.downloadBaseline(10)
+//    crawler.parse("data/0001.xml")
+    crawler.update()
 }
