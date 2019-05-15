5eab4fec062c944c786017e6165924d9c89a1f1a
Nicholas Kapralov <4dvlup@gmail.com>
2019-05-15 15:13:28 +0300

    Added Bootstrap templates and Bokeh subtopic analysis to web-service

--- models/flask-server.py
+++ models/flask-server.py
@@ -1,10 +1,13 @@
-# Hello, Flask!
+from bokeh.embed import components
 from flask import Flask, render_template, request, redirect
+from flask_bootstrap import Bootstrap
 
 from keypaper.analysis import KeyPaperAnalyzer
+from keypaper.visualization import Plotter
 
 app = Flask(__name__)
-analyzer = KeyPaperAnalyzer('nikolay.kapralov@gmail.com')
+Bootstrap(app)
+# app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 
 FORM_HTML = '''<form method="POST">
                   Enter search terms:<br>
@@ -16,18 +19,22 @@ FORM_HTML = '''<form method="POST">
 def index():
     if len(request.args) > 0:
         terms = request.args.get('terms').split('+')
-        analyzer.search(*terms)
-        analyzer.load_publications()
-        return f"Found {len(analyzer.pub_df)} publications:<br>{analyzer.pub_df.to_html()}"
+        analyzer.launch(*terms)
+        data = []
+        for p in plotter.subtopic_timeline_graphs():
+            data.append(components(p))
+        return render_template('results.html', search_string=' '.join(terms), data=data)
 
     if request.method == 'POST':
         terms = request.form.get('terms').split(' ')
         redirect_url = '+'.join(terms)
         return redirect(f'/?terms={redirect_url}')
-    
-    return FORM_HTML
 
-# With debug=True, Flask server will auto-reload 
+    return render_template('main.html')
+
+# With debug=True, Flask server will auto-reload
 # when there are code changes
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    analyzer = KeyPaperAnalyzer()
+    plotter = Plotter(analyzer)
+    app.run(port=5000, debug=True, extra_files=['templates/'])
