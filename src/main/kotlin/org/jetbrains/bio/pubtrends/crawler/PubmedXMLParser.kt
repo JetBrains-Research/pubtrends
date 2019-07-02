0d1e60cf8818d908ea37d1949f79511fd8d6812e
Nicholas Kapralov <4dvlup@gmail.com>
2019-07-02 12:00:18 +0300

    Disable DTD validation, close #63

--- src/main/kotlin/org/jetbrains/bio/pubtrends/crawler/PubmedXMLParser.kt
+++ src/main/kotlin/org/jetbrains/bio/pubtrends/crawler/PubmedXMLParser.kt
@@ -1,10 +1,14 @@
 package org.jetbrains.bio.pubtrends.crawler
 
 import org.apache.logging.log4j.LogManager
+import org.xml.sax.EntityResolver
 import org.xml.sax.InputSource
 import org.xml.sax.SAXException
 import java.io.File
 import javax.xml.parsers.SAXParserFactory
+import java.io.StringReader
+import java.io.IOException
+
 
 class PubmedXMLParser(val pubmedXMLHandler: PubmedXMLHandler) {
     companion object {
@@ -22,6 +26,7 @@ class PubmedXMLParser(val pubmedXMLHandler: PubmedXMLHandler) {
 
     init {
         xmlReader.contentHandler = pubmedXMLHandler
+        xmlReader.entityResolver = NoDTDEntityResolver()
     }
 
     fun parse(name: String): Boolean {
@@ -37,5 +42,19 @@ class PubmedXMLParser(val pubmedXMLHandler: PubmedXMLHandler) {
         }
 
         return true
+    }
+}
+
+class NoDTDEntityResolver : EntityResolver {
+    /**
+     * Overrides default EntityResolver to avoid downloading DTDs for every XML while parsing.
+     */
+    @Throws(SAXException::class, IOException::class)
+    override fun resolveEntity(publicId: String?, systemId: String): InputSource? {
+        return if (systemId.contains("dtd.nlm.nih.gov")) {
+            InputSource(StringReader(""))
+        } else {
+            null
+        }
     }
 }
