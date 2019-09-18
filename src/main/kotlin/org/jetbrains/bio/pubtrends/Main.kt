9aa22c31a54097de0745de76596500d2c82d70f0
Nicholas Kapralov <4dvlup@gmail.com>
2019-09-18 23:41:24 +0300

    Fixed credentials

--- src/main/kotlin/org/jetbrains/bio/pubtrends/Main.kt
+++ src/main/kotlin/org/jetbrains/bio/pubtrends/Main.kt
@@ -1,6 +1,13 @@
 package org.jetbrains.bio.pubtrends
 
 import org.jetbrains.bio.pubtrends.pm.Neo4jDatabaseHandler
+import java.io.BufferedReader
+import java.io.FileReader
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.util.*
+import kotlin.system.exitProcess
 
 /**
  * @author Oleg Shpynov
@@ -11,8 +18,26 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             println("Welcome to Pubtrends!")
-            val dbHandler = Neo4jDatabaseHandler("localhost", 7687, "neo4j",
-                    "deve1oper", false)
+
+            // Configure settings folder
+            val settingsRoot: Path = Paths.get(System.getProperty("user.home", ""), ".pubtrends")
+            check(Files.exists(settingsRoot)) {
+                "$settingsRoot should have been created by log4j"
+            }
+            println("Settings folder $settingsRoot")
+
+            val configPath: Path = settingsRoot.resolve("config.properties")
+            if (Files.notExists(configPath)) {
+                println("Config file not found, please modify and copy config.properties to $configPath")
+                exitProcess(1)
+            }
+
+            val config = Properties().apply {
+                load(BufferedReader(FileReader(configPath.toFile())))
+            }
+
+            val dbHandler = Neo4jDatabaseHandler(config["neo4jurl"].toString(), 7687,
+                    config["neo4jusername"].toString(), config["neo4jpassword"].toString(), false)
             dbHandler.init()
             dbHandler.close()
         }
