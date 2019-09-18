63dee96f8256f3fcb05928df0e10e0f83ceca8c0
Nicholas Kapralov <4dvlup@gmail.com>
2019-09-18 23:30:33 +0300

    First experiments with neo4j Java driver

--- src/main/kotlin/org/jetbrains/bio/pubtrends/Main.kt
+++ src/main/kotlin/org/jetbrains/bio/pubtrends/Main.kt
@@ -1,5 +1,7 @@
 package org.jetbrains.bio.pubtrends
 
+import org.jetbrains.bio.pubtrends.pm.Neo4jDatabaseHandler
+
 /**
  * @author Oleg Shpynov
  * @date 2019-07-22
@@ -9,6 +11,10 @@ class Main {
         @JvmStatic
         fun main(args: Array<String>) {
             println("Welcome to Pubtrends!")
+            val dbHandler = Neo4jDatabaseHandler("localhost", 7687, "neo4j",
+                    "deve1oper", false)
+            dbHandler.init()
+            dbHandler.close()
         }
     }
 }
