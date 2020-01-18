470b61f461dbb8750f95c916a5ba7a69240ee98d
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-01-18 13:17:36 +0300

    Missing cleanup step

--- dist.sh
+++ dist.sh
@@ -33,7 +33,7 @@ rm -r dist
 mkdir -p dist
 tar -zcvf "dist/${PTV}.tar.gz" ${PTV}
 # Cleanup
-rm -f pubtrends
+rm -f ${PTV}
 
 # Move jar to dist if exists
 if [[ -f build/libs/pubtrends-dev.jar ]]; then
