c71e9a1475fd6169c317c6b09a13d023cfbde33b
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-01-18 13:16:58 +0300

    Don't put build time into version

--- dist.sh
+++ dist.sh
@@ -18,23 +18,24 @@ done
 
 
 VERSION_BUILD="${version}.${build}"
-FULL_VERSION="${VERSION_BUILD} built on $(date)"
+FULL_VERSION="${VERSION_BUILD} built on $(date +%F)"
+PTV=pubtrends-${VERSION_BUILD}
 
 # Update config version
-mkdir -p pubtrends
-cp config.properties pubtrends/
-sed -E "s/version = [^\n]*/version = ${FULL_VERSION}/g"     -i pubtrends/config.properties
-cp docker-compose.yml pubtrends/
-cp -r models pubtrends/
+mkdir -p ${PTV}
+cp config.properties ${PTV}/
+sed -E "s/version = [^\n]*/version = ${FULL_VERSION}/g" -i ${PTV}/config.properties
+cp docker-compose.yml ${PTV}/
+cp -r models ${PTV}/
 
 # Create distributive tar.gz
 rm -r dist
 mkdir -p dist
-tar -zcvf "dist/pubtrends-${VERSION_BUILD}.tar.gz" pubtrends
+tar -zcvf "dist/${PTV}.tar.gz" ${PTV}
 # Cleanup
 rm -f pubtrends
 
 # Move jar to dist if exists
 if [[ -f build/libs/pubtrends-dev.jar ]]; then
-  mv build/libs/pubtrends-dev.jar "dist/pubtrends-${VERSION_BUILD}.jar"
+  mv build/libs/pubtrends-dev.jar "dist/${PTV}.jar"
 fi
