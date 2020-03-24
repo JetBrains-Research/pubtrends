d979ada25a82f5b801469844eb60a13f450d7f67
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-03-25 00:28:56 +0300

    Support alternative searches

--- models/keypaper/utils.py
+++ models/keypaper/utils.py
@@ -307,14 +307,18 @@ def trim(string, max_length):
 
 def preprocess_search_query(query, min_search_words):
     """ Preprocess search string for Neo4j full text lookup """
+    if ',' in query:
+        return '"' + ' OR '.join(
+            [f'({preprocess_search_query(p.strip(), min_search_words)[1:-1]})' for p in query.split(',')]
+        ) + '"'
     processed = re.sub('[ ]{2,}', ' ', query.strip())  # Whitespaces normalization, see #215
     if len(processed) == 0:
         raise Exception('Empty query')
-    processed = re.sub('[^0-9a-zA-Z\'"\\-\\.+, ]', '', processed)  # Remove unknown symbols
+    processed = re.sub('[^0-9a-zA-Z\'"\\-\\.+ ]', '', processed)  # Remove unknown symbols
     if len(processed) == 0:
         raise Exception('Illegal character(s), only English letters, numbers, '
                         f'and +- signs are supported. Query: {query}')
-    if len(processed.split(' ')) < min_search_words:
+    if len(re.split('[ -]', processed)) < min_search_words:
         raise Exception(f'Please use more specific query with >= {min_search_words} words. Query: {query}')
     # Looking for complete phrase
     if re.match('^"[^"]+"$', processed):
@@ -323,7 +327,7 @@ def preprocess_search_query(query, min_search_words):
         words = [re.sub("'s$", '', w) for w in processed.split(' ')]  # Fix apostrophes
         stemmer = SnowballStemmer('english')
         stems = set([stemmer.stem(word) for word in words])
-        if len(stems) < min_search_words:
+        if len(stems) + len(processed.split('-')) - 1 < min_search_words:
             raise Exception(f'Please use query with >= {min_search_words} different words. Query: {query}')
         return '"' + ' AND '.join([f"'{w}'" for w in words]) + '"'
     raise Exception(f'Illegal search query, please use search terms or '
--- models/test/test_utils.py
+++ models/test/test_utils.py
@@ -62,7 +62,8 @@ class TestUtils(unittest.TestCase):
         ('"Foo Bar"', '\'"Foo Bar"\''),
         ('Foo-Bar', '"\'Foo-Bar\'"'),
         ('&^Foo-Bar', '"\'Foo-Bar\'"'),
-        ("Alzheimer's disease", '"\'Alzheimer\' AND \'disease\'"')
+        ("Alzheimer's disease", '"\'Alzheimer\' AND \'disease\'"'),
+        ('Foo, Bar', '"(\'Foo\') OR (\'Bar\')"')
     ])
     def test_preprocess_search_valid_source(self, terms, expected):
         self.assertEqual(expected, preprocess_search_query(terms, 0))
@@ -79,6 +80,17 @@ class TestUtils(unittest.TestCase):
     def test_preprocess_search_too_few_words_stems(self):
         with self.assertRaises(Exception):
             preprocess_search_query('Humans Humanity', 2)
+
+    def test_preprocess_search_dash_split(self):
+        self.assertEqual('"\'Covid-19\'"', preprocess_search_query('Covid-19', 2))
+
+    def test_preprocess_search_or(self):
+        self.assertEqual(
+            '"("COVID-19") OR (\'Coronavirus\') OR ("Corona virus") OR ("2019-nCoV") OR ("SARS-CoV") OR ("MERS-CoV") '
+            'OR ("Severe Acute Respiratory Syndrome") OR ("Middle East Respiratory Syndrome")"',
+            preprocess_search_query('"COVID-19", Coronavirus, "Corona virus", "2019-nCoV", "SARS-CoV", '
+                                    '"MERS-CoV", "Severe Acute Respiratory Syndrome", '
+                                    '"Middle East Respiratory Syndrome"', 0))
 
     def test_preprocess_search_illegal_string(self):
         with self.assertRaises(Exception):
