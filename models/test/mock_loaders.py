ed279062d06845918bc3379e6c356662a30209cc
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-04-06 23:01:30 +0300

    Testdata fixed

--- models/test/mock_loaders.py
+++ models/test/mock_loaders.py
@@ -37,11 +37,11 @@ BIBLIOGRAPHIC_COUPLING_DATA = [['3', '4', 2],
                                ['4', '5', 2]]
 
 SIMILARITY_GRAPH_EDGES = [('1', '2', {'cocitation': 3}),
-                          ('1', '4', {'citation': 1}),
+                          ('1', '4', {'citation': 1, 'text': 0.6279137616509934}),
                           ('1', '3', {'citation': 1}),
                           ('1', '5', {'citation': 1}),
                           ('2', '4', {'citation': 1}),
-                          ('2', '3', {'citation': 1}),
+                          ('2', '3', {'citation': 1, 'text': 1.0}),
                           ('2', '5', {'citation': 1}),
                           ('3', '4', {'cocitation': 1, 'bibcoupling': 2}),
                           ('3', '5', {'bibcoupling': 2, 'citation': 1}),
