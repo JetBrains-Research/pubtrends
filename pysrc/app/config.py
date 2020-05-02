6b9c517290790a605ebd247ba139bf8206479b9c
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-05-02 20:33:19 +0300

    Files missing in previous commit: Add /admin login page to view statistics. https://github.com/JetBrains-Research/pubtrends/commit/f9808029463628e5cefae67c4614e165b10bc887

---
+++ pysrc/app/config.py
@@ -0,0 +1,26 @@
+# Create dummy secret key so we can use sessions
+SECRET_KEY = '123456790'
+
+# Create in-memory database
+DATABASE_FILE = 'db.sqlite'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + DATABASE_FILE
+SQLALCHEMY_ECHO = False
+
+# Flask-Security config
+SECURITY_URL_PREFIX = "/admin"
+SECURITY_PASSWORD_HASH = "pbkdf2_sha512"
+SECURITY_PASSWORD_SALT = "D82CWwxNDzdhB7mbeaAChVd2BjdM9VjR"
+
+# # Flask-Security URLs, overridden because they don't put a / at the end
+SECURITY_LOGIN_URL = "/login/"
+SECURITY_LOGOUT_URL = "/logout/"
+# SECURITY_REGISTER_URL = "/register/"
+
+SECURITY_POST_LOGIN_VIEW = "/admin/stats/"
+SECURITY_POST_LOGOUT_VIEW = "/"
+# SECURITY_POST_REGISTER_VIEW = "/admin/"
+
+# Flask-Security features
+SECURITY_REGISTERABLE = False
+SECURITY_SEND_REGISTER_EMAIL = False
+SQLALCHEMY_TRACK_MODIFICATIONS = False
