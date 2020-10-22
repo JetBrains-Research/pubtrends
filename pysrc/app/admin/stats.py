02c624709ae0a8d1cab5c4f04eb09c76e6356b2e
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-10-22 10:35:51 +0300

    Improved statistics: show number of successful reports, avg, time, /papers and /graph usages statistics

--- pysrc/app/admin/stats.py
+++ pysrc/app/admin/stats.py
@@ -19,57 +19,143 @@ RECENT_SEARCHES = 100
 
 
 def prepare_stats_data(logfile):
-    terms_searches = []
-    paper_searches = []
+    terms_searches_dates = []
+    paper_searches_dates = []
     recent_searches = Queue(maxsize=RECENT_SEARCHES)
+    searches_infos = dict()
     terms = []
 
     for line in open(logfile).readlines():
-        if 'INFO' not in line:
-            continue
-        search_date = re.search('[\\d-]+ [\\d:]+,\\d+', line)
-        if search_date is None:
-            continue
-        date = datetime.datetime.strptime(search_date.group(0), '%Y-%m-%d %H:%M:%S,%f')
-        if '/process regular search addr:' in line:
-            terms_searches.append(date)
-            if recent_searches.full():
-                recent_searches.get()  # Free some space
-            terms.append(re.sub('(.*"query": ")|(", "source.*)', '', line.strip()))
-            recent_searches.put((date, re.sub(".*args:", "", line.strip())))
-        if '/search_paper addr:' in line:
-            paper_searches.append(date)
+        try:
+            if 'INFO' not in line:
+                continue
+            search_date = re.search('[\\d-]+ [\\d:]+,\\d+', line)
+            if search_date is None:
+                continue
+            date = datetime.datetime.strptime(search_date.group(0), '%Y-%m-%d %H:%M:%S,%f')
+            if '/process regular search addr:' in line:
+                terms_searches_dates.append(date)
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                terms.append(args['query'].replace(',', ' ').replace('[^a-zA-Z0-9]+', ' '))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                searches_infos[jobid] = dict(jobid=jobid, start=date, args=args)
+                if recent_searches.full():
+                    recent_searches.get()  # Free some space
+                recent_searches.put(jobid)
+            if '/result success addr:' in line:
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                if jobid in searches_infos:
+                    info = searches_infos[jobid]
+                    if 'end' not in info:
+                        info['end'] = date
+            if '/papers success addr:' in line:
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                if jobid in searches_infos:
+                    info = searches_infos[jobid]
+                    if 'papers' not in info:
+                        info['papers'] = True
+            if '/graph success citations addr:' in line:
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                if jobid in searches_infos:
+                    info = searches_infos[jobid]
+                    if 'graph_citations' not in info:
+                        info['graph_citations'] = True
+            if '/graph success structure addr:' in line:
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                if jobid in searches_infos:
+                    info = searches_infos[jobid]
+                    if 'graph_structure' not in info:
+                        info['graph_structure'] = True
+            if '/search_paper addr:' in line:
+                paper_searches_dates.append(date)
+        except:
+            pass  # Ignore single line error
 
+    # Build timeseries graphs for terms and papers searches
     result = {}
-    total_terms_searches = len(terms_searches)
+    total_terms_searches = len(terms_searches_dates)
     result['total_terms_searches'] = total_terms_searches
     if total_terms_searches:
-        p = prepare_timeseries(terms_searches, 'Terms searches')
+        p = prepare_timeseries(terms_searches_dates, 'Terms searches')
         result['terms_searches_plot'] = [components(p)]
 
-    total_paper_searches = len(paper_searches)
+    total_paper_searches = len(paper_searches_dates)
     result['total_paper_searches'] = total_paper_searches
     if total_paper_searches:
-        p = prepare_timeseries(paper_searches, 'Paper searches')
+        p = prepare_timeseries(paper_searches_dates, 'Paper searches')
         result['paper_searches_plot'] = [components(p)]
 
-    recent_searches_query_links = []
+    # Process recent searches
+    recent_searches_results = []
     while not recent_searches.empty():
-        date, rs = recent_searches.get()
-        args_map = json.loads(rs)
-        query = args_map['query']
-        link = f'/result?{"&".join([f"{a}={v}" for a, v in args_map.items()])}'
-        recent_searches_query_links.append((date.strftime('%Y-%m-%d'), query, link))
-    result['recent_searches'] = recent_searches_query_links[::-1]
+        jobid = recent_searches.get()
+        info = searches_infos[jobid]
+        date, args = info['start'], info['args']
+        if 'end' in info:
+            duration = duration_string(info['end'] - date)
+        else:
+            duration = '-'
 
-    # Generate a word cloud image
+        query = args['query']
+        link = f'/result?{"&".join([f"{a}={v}" for a, v in args.items()])}'
+        recent_searches_results.append((
+            date.strftime('%Y-%m-%d'),
+            query,
+            link,
+            duration,
+            '+' if 'papers' in info else '-',
+            '+' if 'graph_citations' in info else '-',
+            '+' if 'graph_structure' in info else '-',
+        ))
+    result['recent_searches'] = recent_searches_results[::-1]
+
+    # Generate a word cloud
     text = ' '.join(terms).replace(',', ' ').replace('[^a-zA-Z0-9]+', ' ')
     if text:  # Check that string is not empty
         wc = WordCloud(collocations=False, width=PLOT_WIDTH - 40, height=WC_HEIGHT - 40,
                        background_color='white', max_font_size=100).generate(text)
         result['word_cloud'] = Plotter.word_cloud_prepare(wc)
 
+    # General statistics
+    successful_searches = sum('end' in info for info in searches_infos.values())
+    result['successful_searches'] = successful_searches
+    duration_secs = 0
+    for info in searches_infos.values():
+        if 'end' in info:
+            duration_secs += (info['end'] - info['start']).seconds
+    if successful_searches > 0:
+        result['searches_avg_duration'] = duration_string(
+            datetime.timedelta(seconds=int(duration_secs / successful_searches))
+        )
+    else:
+        result['searches_avg_duration'] = 'N/A'
+    result['searches_papers_shown'] = sum('papers' in info for info in searches_infos.values())
+    result['searches_graph_citations_shown'] = sum('graph_citations' in info for info in searches_infos.values())
+    result['searches_graph_structure_shown'] = sum('graph_structure' in info for info in searches_infos.values())
+
     return result
+
+
+def duration_string(dt):
+    dt = dt.seconds
+    h = divmod(dt, 86400 * 3600)  # hours
+    m = divmod(h[1], 60)  # minutes
+    s = m[1]  # seconds
+    return f'{h[0]}h {m[0]}m {s}s'
 
 
 def prepare_timeseries(dates, title):
