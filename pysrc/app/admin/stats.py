a919eee20421b083956acfedebf1be881e4e847d
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-10-23 20:49:39 +0300

    Stats for paper searches: successful and avg running time

--- pysrc/app/admin/stats.py
+++ pysrc/app/admin/stats.py
@@ -15,15 +15,18 @@ TOOLS = "hover,pan,tap,wheel_zoom,box_zoom,reset,save"
 PLOT_WIDTH = 900
 PLOT_HEIGHT = 300
 WC_HEIGHT = 600
-RECENT_SEARCHES = 100
+RECENT = 50
 
 
 def prepare_stats_data(logfile):
     terms_searches_dates = []
-    paper_searches_dates = []
-    recent_searches = Queue(maxsize=RECENT_SEARCHES)
+    recent_searches = Queue(maxsize=RECENT)
     searches_infos = dict()
     terms = []
+
+    paper_searches_dates = []
+    recent_papers = Queue(maxsize=RECENT)
+    papers_infos = dict()
 
     for line in open(logfile).readlines():
         try:
@@ -80,8 +83,25 @@ def prepare_stats_data(logfile):
                     info = searches_infos[jobid]
                     if 'graph_structure' not in info:
                         info['graph_structure'] = True
-            if '/search_paper addr:' in line:
+            if '/process paper analysis addr:' in line:
                 paper_searches_dates.append(date)
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                papers_infos[jobid] = dict(jobid=jobid, start=date, args=args)
+                if recent_papers.full():
+                    recent_papers.get()  # Free some space
+                recent_papers.put(jobid)
+            if '/paper success addr:' in line:
+                args = json.loads(re.sub(".*args:", "", line.strip()))
+                jobid = args['jobid']
+                if not jobid:
+                    continue
+                if jobid in papers_infos:
+                    info = papers_infos[jobid]
+                    if 'end' not in info:
+                        info['end'] = date
         except:
             pass  # Ignore single line error
 
@@ -110,11 +130,10 @@ def prepare_stats_data(logfile):
         else:
             duration = '-'
 
-        query = args['query']
         link = f'/result?{"&".join([f"{a}={v}" for a, v in args.items()])}'
         recent_searches_results.append((
             date.strftime('%Y-%m-%d %H:%M:%S'),
-            query,
+            args['query'],
             link,
             duration,
             '+' if 'papers' in info else '-',
@@ -123,6 +142,26 @@ def prepare_stats_data(logfile):
         ))
     result['recent_searches'] = recent_searches_results[::-1]
 
+    # Process recent papers
+    recent_papers_results = []
+    while not recent_papers.empty():
+        jobid = recent_papers.get()
+        info = papers_infos[jobid]
+        date, args = info['start'], info['args']
+        if 'end' in info:
+            duration = duration_string(info['end'] - date)
+        else:
+            duration = '-'
+
+        link = f'/result?{"&".join([f"{a}={v}" for a, v in args.items()])}'
+        recent_papers_results.append((
+            date.strftime('%Y-%m-%d %H:%M:%S'),
+            args['query'],
+            link,
+            duration,
+        ))
+    result['recent_papers'] = recent_papers_results[::-1]
+
     # Generate a word cloud
     text = ' '.join(terms).replace(',', ' ').replace('[^a-zA-Z0-9]+', ' ')
     if text:  # Check that string is not empty
@@ -130,22 +169,38 @@ def prepare_stats_data(logfile):
                        background_color='white', max_font_size=100).generate(text)
         result['word_cloud'] = Plotter.word_cloud_prepare(wc)
 
-    # General statistics
+    # Terms search statistics
     successful_searches = sum('end' in info for info in searches_infos.values())
     result['successful_searches'] = successful_searches
-    duration_secs = 0
+    duration = 0
     for info in searches_infos.values():
         if 'end' in info:
-            duration_secs += (info['end'] - info['start']).seconds
+            duration += (info['end'] - info['start']).seconds
     if successful_searches > 0:
         result['searches_avg_duration'] = duration_string(
-            datetime.timedelta(seconds=int(duration_secs / successful_searches))
+            datetime.timedelta(seconds=int(duration / successful_searches))
         )
     else:
         result['searches_avg_duration'] = 'N/A'
     result['searches_papers_shown'] = sum('papers' in info for info in searches_infos.values())
     result['searches_graph_citations_shown'] = sum('graph_citations' in info for info in searches_infos.values())
     result['searches_graph_structure_shown'] = sum('graph_structure' in info for info in searches_infos.values())
+
+    # Papers search statistics
+    successful_paper_searches = sum('end' in info for info in papers_infos.values())
+    result['paper_successful_searches'] = successful_paper_searches
+    duration = 0
+    for info in papers_infos.values():
+        if 'end' in info:
+            duration += (info['end'] - info['start']).seconds
+    if successful_paper_searches > 0:
+        result['paper_searches_avg_duration'] = duration_string(
+            datetime.timedelta(seconds=int(duration / successful_paper_searches))
+        )
+    else:
+        result['paper_searches_avg_duration'] = 'N/A'
+
+    result['recent'] = RECENT
 
     return result
 
@@ -155,7 +210,7 @@ def duration_string(dt):
     h = divmod(dt, 86400 * 3600)  # hours
     m = divmod(h[1], 60)  # minutes
     s = m[1]  # seconds
-    return f'{h[0]}h {m[0]}m {s}s'
+    return f'{h[0]:02d}:{m[0]:02d}:{s:02d}'
 
 
 def prepare_timeseries(dates, title):
