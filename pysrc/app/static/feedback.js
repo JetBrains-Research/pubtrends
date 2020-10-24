7f4f3a8cde2237c3450b03e08f24c91e40f2ae32
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2020-10-24 22:41:57 +0300

    1. Notify to use About for quick introduction 2. Switched to cdn instead of local resources for notify.js

--- pysrc/app/static/feedback.js
+++ pysrc/app/static/feedback.js
@@ -1,4 +1,9 @@
 "use strict";
+// Include this snippet to include notify.js 0.4.2
+//     <!-- Notify.js -->
+//       <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js"
+//               integrity="sha512-efUTj3HdSPwWJ9gjfGR71X9cvsrthIA78/Fvd/IN+fttQVy7XWkOAXb295j8B3cmm/kFKVxjiNYzKw9IQJHIuQ=="
+//               crossorigin="anonymous"></script>
 
 function clearSelection($buttons) {
     $buttons
