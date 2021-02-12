4e235cb77ab8b7ed24b60f04c4464c6bd34aafac
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-02-12 17:33:33 +0300

    Introduce review lock

--- pysrc/review/app/review.py
+++ pysrc/review/app/review.py
@@ -3,6 +3,7 @@ import os
 
 import torch
 from lazy import lazy
+from threading import Lock
 
 from pysrc.papers.analyzer import KeyPaperAnalyzer
 from pysrc.papers.pubtrends_config import PubtrendsConfig
@@ -41,37 +42,43 @@ class ModelCache:
 
 MODEL_CACHE = ModelCache()
 
+REVIEW_LOCK = Lock()
+
 
 def generate_review(data, source, num_papers, num_sents, progress, task):
-    progress.info(f'Generating review', current=1, task=task)
-    loader, url_prefix = Loaders.get_loader_and_url_prefix(source, PUBTRENDS_CONFIG)
-    analyzer = KeyPaperAnalyzer(loader, PUBTRENDS_CONFIG)
-    analyzer.init(data)
-    progress.info('Initializing model and device', current=2, task=task)
-    model, device = MODEL_CACHE.model_and_device
-    progress.info('Configuring model for evaluation', current=3, task=task)
-    model.eval()
-    top_cited_papers, top_cited_df = analyzer.find_top_cited_papers(
-        analyzer.df, n_papers=int(num_papers)
-    )
-    progress.info(f'Processing abstracts for {len(top_cited_papers)} top cited papers', current=4, task=task)
-    result = []
-    for pid in top_cited_papers:
-        cur_paper = top_cited_df[top_cited_df['id'] == pid]
-        title = cur_paper['title'].values[0]
-        year = int(cur_paper['year'].values[0])
-        cited = int(cur_paper['total'].values[0])
-        abstract = cur_paper['abstract'].values[0]
-        topic = int(cur_paper['comp'].values[0] + 1)
-        data = text_to_data(abstract, 512, model.tokenizer)
-        choose_from = []
-        for article_ids, article_mask, article_seg, magic, sents in data:
-            input_ids = torch.tensor([article_ids]).to(device)
-            input_mask = torch.tensor([article_mask]).to(device)
-            input_segment = torch.tensor([article_seg]).to(device)
-            draft_probs = model(input_ids, input_mask, input_segment, )
-            choose_from.extend(zip(sents[magic:], draft_probs.cpu().detach().numpy()[magic:]))
-        to_add = sorted(choose_from, key=lambda x: -x[1])[:int(num_sents)]
-        for sent, score in to_add:
-            result.append([title, year, cited, topic, sent, url_prefix + pid, float("{:.2f}".format(score))])
-    return result
+    try:
+        REVIEW_LOCK.acquire()
+        progress.info(f'Generating review', current=1, task=task)
+        loader, url_prefix = Loaders.get_loader_and_url_prefix(source, PUBTRENDS_CONFIG)
+        analyzer = KeyPaperAnalyzer(loader, PUBTRENDS_CONFIG)
+        analyzer.init(data)
+        progress.info('Initializing model and device', current=2, task=task)
+        model, device = MODEL_CACHE.model_and_device
+        progress.info('Configuring model for evaluation', current=3, task=task)
+        model.eval()
+        top_cited_papers, top_cited_df = analyzer.find_top_cited_papers(
+            analyzer.df, n_papers=int(num_papers)
+        )
+        progress.info(f'Processing abstracts for {len(top_cited_papers)} top cited papers', current=4, task=task)
+        result = []
+        for pid in top_cited_papers:
+            cur_paper = top_cited_df[top_cited_df['id'] == pid]
+            title = cur_paper['title'].values[0]
+            year = int(cur_paper['year'].values[0])
+            cited = int(cur_paper['total'].values[0])
+            abstract = cur_paper['abstract'].values[0]
+            topic = int(cur_paper['comp'].values[0] + 1)
+            data = text_to_data(abstract, 512, model.tokenizer)
+            choose_from = []
+            for article_ids, article_mask, article_seg, magic, sents in data:
+                input_ids = torch.tensor([article_ids]).to(device)
+                input_mask = torch.tensor([article_mask]).to(device)
+                input_segment = torch.tensor([article_seg]).to(device)
+                draft_probs = model(input_ids, input_mask, input_segment, )
+                choose_from.extend(zip(sents[magic:], draft_probs.cpu().detach().numpy()[magic:]))
+            to_add = sorted(choose_from, key=lambda x: -x[1])[:int(num_sents)]
+            for sent, score in to_add:
+                result.append([title, year, cited, topic, sent, url_prefix + pid, float("{:.2f}".format(score))])
+        return result
+    finally:
+        REVIEW_LOCK.release()
