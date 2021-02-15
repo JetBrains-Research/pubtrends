0dbccf99553b75f556efb34532a5f2187908eea7
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-02-15 12:06:05 +0300

    Feedback procedure cleanup

--- pysrc/app/static/feedback.js
+++ pysrc/app/static/feedback.js
@@ -5,39 +5,70 @@
 //               integrity="sha512-efUTj3HdSPwWJ9gjfGR71X9cvsrthIA78/Fvd/IN+fttQVy7XWkOAXb295j8B3cmm/kFKVxjiNYzKw9IQJHIuQ=="
 //               crossorigin="anonymous"></script>
 
-function clearSelection($buttons) {
-    $buttons
-        .removeClass('btn-feedback-yes-selected')
-        .removeClass('btn-feedback-meh-selected')
-        .removeClass('btn-feedback-no-selected');
+function createClickableFeedBackForm(feedBackElementId, feedBackId) {
+    createClickableFeedBackFormImpl(feedBackElementId, feedBackId, 'feedback');
+}
+
+function createClickableFeedBackFormNoMessage(feedBackElementId, feedBackId) {
+    createClickableFeedBackFormImpl(feedBackElementId, feedBackId, 'feedbackNoMessageForm');
+}
+
+function createClickableFeedBackFormImpl(feedBackElementId, feedBackId, feedbackFunction) {
+    $('#' + feedBackElementId).html(`
+        <small className="text-muted">Was this useful?</small>
+        <div id="` + feedBackId + `" className="btn-group-horizontal">
+            <button type="button" className="btn btn-sm btn-feedback-yes"
+                onClick="` + feedbackFunction + `(this, 1)">
+                <img src="smile.svg" alt="Yes"/>
+            </button>
+            <button type="button" className="btn btn-sm btn-feedback-meh"
+                onClick="` + feedbackFunction + `(this, 0)">
+                <img src="meh.svg" alt="Not sure"/>
+            </button>
+            <button type="button" className="btn btn-sm btn-feedback-no"
+                onClick="` + feedbackFunction + `(this, -1)">
+                <img src="frown.svg" alt="No"/>
+            </button>
+        </div>
+    `)
 }
 
 // element - feedback button
 //      element should be placed under class btn-group-horizontal, which id is used as key
 // value -1, 0, +1
 function feedback(element, value) {
-    feedbackImpl(element, value, true)
+    feedbackImpl(element, value)
+    $.notify("Thanks! You can leave us a message below.", {
+        className: "success",
+        position: "bottom right"
+    });
 }
 
 function feedbackNoMessageForm(element, value) {
-    feedbackImpl(element, value, false)
+    feedbackImpl(element, value)
+    $.notify("Thanks for the feedback!", {
+        className: "success",
+        position: "bottom right"
+    });
 }
 
-function feedbackImpl(element, value, messageFormIsAvailable) {
+function feedbackImpl(element, value) {
     let $element = $(element);
-    let groupId = $element.closest('.btn-group-horizontal').attr('id');
-    let key = groupId;
-    console.info("Feedback " + groupId + ": " + value);
-    let $buttons = $('#' + groupId + ' button');
+    let feedBackId = $element.closest('.btn-group-horizontal').attr('id');
+    console.info("Feedback " + feedBackId + ": " + value);
+    let $buttons = $('#' + feedBackId + ' button');
     ['yes', 'meh', 'no'].forEach((opt) => {
         const optClass = 'btn-feedback-' + opt;
         const selectedClass = 'btn-feedback-' + opt + '-selected';
         if ($element.hasClass(optClass)) {
             if ($element.hasClass(selectedClass)) {
                 $element.removeClass(selectedClass);
-                key = 'cancel:' + key;
+                feedBackId = 'cancel:' + feedBackId;
             } else {
-                clearSelection($buttons);
+                $buttons
+                    .removeClass('btn-feedback-yes-selected')
+                    .removeClass('btn-feedback-meh-selected')
+                    .removeClass('btn-feedback-no-selected');
                 $element.addClass(selectedClass);
             }
         }
@@ -49,7 +80,7 @@ function feedbackImpl(element, value, messageFormIsAvailable) {
         type: "POST",
         data: {
             jobid: jobid,
-            key: key,
+            key: feedBackId,
             value: value
         },
 
@@ -61,16 +92,6 @@ function feedbackImpl(element, value, messageFormIsAvailable) {
             const responseText = XMLHttpRequest.responseText;
             console.error("Error recording feedback: " + responseText);
         }
-    });
-    let message;
-    if (messageFormIsAvailable) {
-        message = "Thanks! You can leave us a message below.";
-    } else {
-        message = "Thanks for the feedback!";
-    }
-    $.notify(message, {
-        className: "success",
-        position: "bottom right"
     });
 }
 
