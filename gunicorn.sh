e35d6a88d5af342885438e3812de2aef6fb16241
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-07-16 14:57:55 +0300

    Fix SSL params are not detected corrected when certificate files present. Close #262

---
+++ gunicorn.sh
@@ -0,0 +1,15 @@
+# Script to launch gunicorn with optional SSL if key files present.
+# Use it in CMD file, because Docker doesn't support conditional CMD.
+
+# NOTE: Use --limit-request-line to support huge lists of papers
+
+if [[ -f /ssl/privkey.pem ]]; then
+  echo "SSL keys found. Starting gunicorn with SSL support."
+  gunicorn --bind 0.0.0.0:8888 --workers 5 --limit-request-line 0 \
+    --log-level=info --log-file=/logs/gunicorn.log "pysrc.app.app:get_app()" \
+    --keyfile=/ssl/privkey.pem --certfile=/ssl/cert.pem --ca-certs=/ssl/chain.pem
+else
+  echo "No SSL keys found. Starting gunicorn without SSL support."
+  gunicorn --bind 0.0.0.0:8888 --workers 5 --limit-request-line 0 \
+    --log-level=info --log-file=/logs/gunicorn.log "pysrc.app.app:get_app()"
+fi
