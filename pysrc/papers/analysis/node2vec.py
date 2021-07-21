3369a1b7920b339b1e5671bc03d24564f170a903
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-07-21 10:37:13 +0300

    Progress logging for node2vec

--- pysrc/papers/analysis/node2vec.py
+++ pysrc/papers/analysis/node2vec.py
@@ -10,22 +10,24 @@ tf.get_logger().setLevel('ERROR')
 logger = logging.getLogger(__name__)
 
 
-def node2vec(graph, p=0.5, q=2.0, walk_length=64, walks_per_node=10, vector_size=64, seed=None):
+def node2vec(graph, p=0.5, q=2.0, walk_length=64, walks_per_node=10, vector_size=64, key='weight', seed=None):
     """
-    :param graph: Undirected networkx graph, 'weight' value of each edge indicates similarity
+    :param graph: Undirected weighted networkx graph
     :param p: Defines (unormalised) probability, 1/p, of returning to source node
     :param q: Defines (unormalised) probability, 1/q, for moving away from source node
     :param walk_length: Walk length for each node. Walk stops preliminary if no neighbors found
     :param walks_per_node: Number of walk actions performed starting from each node
     :param vector_size: Resulting embedding size
+    :param key: key for weight
     :param seed: seed
     :returns indices and matrix of embeddings
     """
-
     logger.debug('Precomputing random walk probabilities')
-    adjacency_map, probabilities1, probabilities2 = _precompute(graph, p=p, q=q)
+    probabilities1, probabilities2 = _precompute(graph, key, p, q)
+
+    logger.debug('Performing random walks')
     walks = _random_walks(
-        list(graph.nodes), adjacency_map, probabilities1, probabilities2,
+        graph, probabilities1, probabilities2,
         walks_per_node, walk_length,
         seed=seed
     )
@@ -38,39 +40,40 @@ def node2vec(graph, p=0.5, q=2.0, walk_length=64, walks_per_node=10, vector_size
     return w2v.wv.index_to_key, w2v.wv.vectors
 
 
-def _precompute(graph, p=0.5, q=2.0):
+def _precompute(graph, key='weight', p=0.5, q=2.0):
     """
-    :param graph: Undirected networkx graph, 'weight' value of each edge indicates similarity
+    :param graph: Undirected weighted networkx graph
+    :param key: weight key for edge
     :param p: Defines (unormalised) probability, 1/p, of returning to source node
     :param q: Defines (unormalised) probability, 1/q, for moving away from source node
-    :return: adjacency map, probabilities on first step, and probabilities on steps 2+
+    :return: probabilities on first step, and probabilities on steps 2+
     """
-    adjacency_map = dict()
     probabilities1 = dict()  # No returning back option
     probabilities2 = {node: dict() for node in graph.nodes()}
 
-    for node in graph.nodes:
+    for i, node in enumerate(graph.nodes()):
+        if i % 100 == 1:
+            logger.debug(f'Analyzed probabilities for {i} nodes')
         neighbors = list(graph.neighbors(node))
-        adjacency_map[node] = neighbors
 
         first_travel_weights = []
         for neighbor in neighbors:
-            first_travel_weights.append(graph[node][neighbor].get('weight', 1))
+            first_travel_weights.append(graph[node][neighbor].get(key, 1))
         probabilities1[node] = _normalize(first_travel_weights)
 
         for neighbor in neighbors:
             walk_weights = []
             for neighbor2 in graph.neighbors(neighbor):
                 if neighbor2 == node:  # Backwards probability
-                    ss_weight = graph[neighbor][neighbor2].get('weight', 1) * 1 / p
+                    ss_weight = graph[neighbor][neighbor2].get(key, 1) * 1 / p
                 elif neighbor2 in graph[node]:  # If the neighbor is connected to the node
-                    ss_weight = graph[neighbor][neighbor2].get('weight', 1)
+                    ss_weight = graph[neighbor][neighbor2].get(key, 1)
                 else:
-                    ss_weight = graph[neighbor][neighbor2].get('weight', 1) * 1 / q
+                    ss_weight = graph[neighbor][neighbor2].get(key, 1) * 1 / q
                 walk_weights.append(ss_weight)
             probabilities2[neighbor][node] = _normalize(walk_weights)
 
-    return adjacency_map, probabilities1, probabilities2
+    return probabilities1, probabilities2
 
 
 def _normalize(values):
@@ -82,8 +85,7 @@ def _normalize(values):
 
 
 def _random_walks(
-        nodes,
-        adjacency_map,
+        graph,
         probabilities1,
         probabilities2,
         walks_per_node,
@@ -93,12 +95,14 @@ def _random_walks(
     np.random.seed(seed)
     walks = []
     for i in range(walks_per_node):
+        logger.debug(f'Generating walk {i+1}')
+
         # Start a random walk from every node
-        for node in nodes:
+        for node in graph.nodes():
             # Perform walk
             walk = [node]
             while len(walk) < walk_length:
-                neighbors = adjacency_map[walk[-1]]
+                neighbors = list(graph.neighbors(walk[-1]))
                 # Dead end nodes
                 if not len(neighbors):
                     break
