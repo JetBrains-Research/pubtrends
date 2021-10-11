bb21d00c10e2464b0845dde0ce3eda79b02063b2
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-10-11 21:55:10 +0300

    Cleanup celery monitor

--- pysrc/app/admin/celery.py
+++ pysrc/app/admin/celery.py
@@ -1,10 +1,13 @@
-import pprint
-
-
 def prepare_celery_data(app):
     inspect = app.control.inspect()
     return dict(
-        active=pprint.pformat(inspect.active()),
-        scheduled=pprint.pformat(inspect.scheduled()),
-        revoked=pprint.pformat(inspect.revoked())
-    )
+        active=pp(list(inspect.active().items())[0][1]),
+        scheduled=pp(list(inspect.reserved().items())[0][1]),
+        revoked=('\n'.join(list(inspect.revoked().items())[0][1])))
+
+
+def pp(tasks):
+    return '\n'.join(
+        f"{t['id']} {t['name']} {','.join(t['args'])} "
+        f"{','.join(str(k) + ':' + str(v) for k, v in t['kwargs'].items())}"
+        for t in tasks)
