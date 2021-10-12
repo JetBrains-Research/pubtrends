7dc9ca598efc5446994dc482ef0efc556f7d8b25
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2021-10-12 23:09:40 +0300

    Cosmetics

--- pysrc/app/utils.py
+++ pysrc/app/utils.py
@@ -4,7 +4,7 @@ MAX_QUERY_LENGTH = 60
 
 SOMETHING_WENT_WRONG_SEARCH = 'Something went wrong, please <a href="/">rerun</a> your search.'
 SOMETHING_WENT_WRONG_TOPIC = 'Something went wrong, please <a href="/">rerun</a> your topic analysis.'
-SOMETHING_WENT_WRONG_PAPER = 'Something went wrong, please <a href="/">rerun</a> your paper analysis.'
+SOMETHING_WENT_WRONG_PAPER = 'Something went wrong, please <a href="/#paper-tab">rerun</a> your paper analysis.'
 ERROR_OCCURRED = "Error occurred. We're working on it. Please check back soon."
 
 
