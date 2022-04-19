09ad7cd803944a644ae4914f6097fbe27872ddb5
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2022-04-19 13:42:22 +0300

    Example on how to fetch papers on the server in Python

---
+++ pysrc/papers/db/fetch_papers.py
@@ -0,0 +1,42 @@
+# Small example how to load papers information from the database
+import psycopg2
+import pandas as pd
+# TODO fill
+host='localhost'
+port='port'
+dbname='dn'
+user='user'
+password='password'
+connection_string = f"""
+                    host={host} \
+                    port={port} \
+                    dbname={dbname} \
+                    user={user} \
+                    password={password}
+                """.strip()
+postgres_connection = psycopg2.connect(connection_string)
+
+query="""
+SELECT P.pmid as id, title, abstract, year, doi, aux
+FROM PMPublications P
+WHERE P.pmid IN (VALUES 
+(30660649),(29895827),(29121253)
+);
+"""
+
+with postgres_connection.cursor() as cursor:
+    cursor.execute(query)
+    df = pd.DataFrame(cursor.fetchall(), columns=['id', 'title', 'abstract', 'year', 'doi', 'aux'], dtype=object)
+
+def extract_authors(authors_list):
+    if not authors_list:
+        return ''
+    return ', '.join(filter(None, map(lambda authors: authors['name'], authors_list)))
+
+
+df['aux'] = df['aux'].apply(lambda aux: json.loads(aux) if type(aux) is str else aux)
+df['authors'] = df['aux'].apply(lambda aux: extract_authors(aux['authors']))
+df['journal'] = df['aux'].apply(lambda aux: aux['journal']['name'])
+df.drop(['aux'], axis=1, inplace=True)
+
+df.to_csv('~/papers.csv')
