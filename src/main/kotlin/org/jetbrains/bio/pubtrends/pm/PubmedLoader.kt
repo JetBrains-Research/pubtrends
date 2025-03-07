74c999a8ddd233e2d9ef9f86bdf562af90f32445
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-03-07 08:51:04 +0100

    Simplified waiting strategy

--- src/main/kotlin/org/jetbrains/bio/pubtrends/pm/PubmedLoader.kt
+++ src/main/kotlin/org/jetbrains/bio/pubtrends/pm/PubmedLoader.kt
@@ -9,17 +9,13 @@ import org.jetbrains.bio.pubtrends.db.AbstractDBWriter
 import org.jetbrains.bio.pubtrends.db.PubmedPostgresWriter
 import org.slf4j.LoggerFactory
 import java.nio.file.Files
-import kotlin.math.min
 import kotlin.system.exitProcess
 
 
 object PubmedLoader {
 
     // 1 minute
-    private const val START_WAIT_TIME = 60
-
-    // 10 minutes
-    private const val MAX_WAIT_TIME = 600
+    private const val WAIT_TIME = 60
 
     private const val MAX_DB_RETRIES = 3
 
@@ -73,7 +69,6 @@ object PubmedLoader {
             val pubmedStatsFile = settingsRoot.resolve("${writerName}_stats.tsv")
 
             var dbRetry = 1
-            var dbWaitTime = START_WAIT_TIME
             var isUpdateRequired = true
             while (isUpdateRequired) {
                 try {
@@ -88,7 +83,6 @@ object PubmedLoader {
                         if (options.has("fillDatabase")) {
                             LOG.info("Checking Pubmed FTP...")
                             var downloadRetry = 1
-                            var downloadWaitTime = START_WAIT_TIME
 
                             LOG.info("Retrying downloading after any problems.")
                             while (isUpdateRequired) {
@@ -108,42 +102,36 @@ object PubmedLoader {
                                         options.valueOf("lastId").toString().toInt()
                                     else
                                         null
-
                                     isUpdateRequired = if (downloadRetry == 1) {
                                         pubmedCrawler.update(lastIdCmd)
                                     } else {
                                         pubmedCrawler.update(null)
                                     }
-                                    downloadWaitTime = START_WAIT_TIME
                                 } catch (e: PubmedCrawlerException) {
                                     LOG.error("Error", e)
                                     isUpdateRequired = true
 
-                                    LOG.info("Download error, retrying in $downloadWaitTime seconds...")
-                                    Thread.sleep(downloadWaitTime * 1000L)
+                                    LOG.info("Download error, retrying in $WAIT_TIME seconds...")
+                                    Thread.sleep(WAIT_TIME * 1000L)
                                     LOG.info("Download retry #$downloadRetry")
                                     downloadRetry += 1
-
-                                    downloadWaitTime = min(downloadWaitTime * 2, MAX_WAIT_TIME)
                                 }
                             }
                             LOG.info("Done crawling.")
                         }
 
                         dbRetry = 1
-                        dbWaitTime = START_WAIT_TIME
                     }
                 } catch (e: Exception) {
-                    LOG.error("Database connection error, retrying in $dbWaitTime seconds...")
+                    LOG.error("Database connection error, retrying in $WAIT_TIME seconds...")
                     dbWriter.close()
                     if (dbRetry > MAX_DB_RETRIES) {
                         LOG.error("Database connection error, maximum retries reached.")
                         exitProcess(1)
                     }
-                    Thread.sleep(dbWaitTime * 1000L)
+                    Thread.sleep(WAIT_TIME * 1000L)
                     LOG.info("Database connection retry #$dbRetry")
                     dbRetry += 1
-                    dbWaitTime = min(dbWaitTime * 2, MAX_WAIT_TIME)
                 }
             }
         }
