a06e10085475958ebdd0876ee013643bca75f756
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-03-11 11:55:07 +0100

    Tweak graph / text embeddings

--- pysrc/papers/config.py
+++ pysrc/papers/config.py
@@ -26,8 +26,7 @@ SIMILARITY_BIBLIOGRAPHIC_COUPLING_MIN = 5
 SIMILARITY_COCITATION_MIN = 2
 
 # Papers embeddings is a concatenation of graph and text embeddings times corresponding factors
-# Graph embeddings produce more clear topics separation, so it goes with higher coefficient
-GRAPH_EMBEDDINGS_FACTOR = 2
+GRAPH_EMBEDDINGS_FACTOR = 1
 TEXT_EMBEDDINGS_FACTOR = 1
 
 # Global vectorization max vocabulary size
