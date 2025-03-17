872daab57d8dce69372d4a677501389474352f87
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-03-17 14:51:18 +0100

    Improved paper dedicated page - better structure, show topics info, etc.

--- pysrc/papers/plot/plotter_paper.py
+++ pysrc/papers/plot/plotter_paper.py
@@ -1,21 +1,25 @@
 import logging
+import json
 from scipy.spatial import distance
 
 from pysrc.papers.analyzer import PapersAnalyzer
 from pysrc.papers.config import PubtrendsConfig
 from pysrc.papers.db.loaders import Loaders
-from pysrc.papers.plot.plotter import Plotter, components_list
-from pysrc.papers.utils import trim, MAX_TITLE_LENGTH
+from pysrc.papers.plot.plotter import Plotter, components_list, TOPIC_KEYWORDS
+from pysrc.papers.utils import trim, MAX_TITLE_LENGTH, topics_palette
 
 PUBTRENDS_CONFIG = PubtrendsConfig(test=False)
 
 logger = logging.getLogger(__name__)
 
 
-def get_top_papers_id_title_year_cited(papers, df, n=50):
+def get_top_papers_id_title_year_cited_topic(papers, df, n=50):
     top_papers = map(lambda v: (df[df['id'] == v], df[df['id'] == v]['total'].values[0]), papers)
-    return [(el[0]['id'].values[0], el[0]['title'].values[0],
-             el[0]['year'].values[0], el[0]['total'].values[0])
+    return [(el[0]['id'].values[0],
+             el[0]['title'].values[0],
+             el[0]['year'].values[0],
+             el[0]['total'].values[0],
+             el[0]['comp'].values[0])
             for el in sorted(top_papers, key=lambda x: x[1], reverse=True)[:n]]
 
 
@@ -39,29 +43,15 @@ def prepare_paper_data(data, source, pid):
     if doi == 'None' or doi == 'nan':
         doi = ''
 
-    logger.debug('Estimate related topics for the paper')
-    if analyzer.sparse_papers_graph.nodes() and analyzer.sparse_papers_graph.has_node(pid):
-        related_topics = {}
-        for v in analyzer.sparse_papers_graph[pid]:
-            c = analyzer.df[analyzer.df['id'] == v]['comp'].values[0]
-            if c in related_topics:
-                related_topics[c] += 1
-            else:
-                related_topics[c] = 1
-        related_topics = map(
-            lambda el: (', '.join(
-                [w[0] for w in analyzer.kwd_df[analyzer.kwd_df['comp'] == el[0]]['kwd'].values[0][:10]]
-            ), el[1]),
-            sorted(related_topics.items(), key=lambda el: el[1], reverse=True)
-        )
-    else:
-        related_topics = None
+    topic_tags = ','.join(
+        [w[0] for w in analyzer.kwd_df[analyzer.kwd_df['comp'] == topic - 1]['kwd'].values[0][:TOPIC_KEYWORDS]]
+    )
 
     logger.debug('Computing most cited prior and derivative papers')
-    derivative_papers = get_top_papers_id_title_year_cited(
+    derivative_papers = get_top_papers_id_title_year_cited_topic(
         analyzer.cit_df.loc[analyzer.cit_df['id_in'] == pid]['id_out'], analyzer.df
     )
-    prior_papers = get_top_papers_id_title_year_cited(
+    prior_papers = get_top_papers_id_title_year_cited_topic(
         analyzer.cit_df.loc[analyzer.cit_df['id_out'] == pid]['id_in'], analyzer.df
     )
 
@@ -74,37 +64,45 @@ def prepare_paper_data(data, source, pid):
                        analyzer.df[analyzer.df['id'] == v]['year'].values[0],
                        analyzer.df[analyzer.df['id'] == v]['total'].values[0],
                        1 / (1 + distance.euclidean(analyzer.papers_embeddings[indx[pid], :],
-                                                   analyzer.papers_embeddings[indx[v], :]))),
+                                                   analyzer.papers_embeddings[indx[v], :])),
+                       analyzer.df[analyzer.df['id'] == v]['comp'].values[0] + 1),
             [p for p in analyzer.df['id'] if p != pid]
         )
         similar_papers = sorted(similar_papers, key=lambda x: x[4], reverse=True)[:50]
     else:
         similar_papers = None
 
-    result = dict(title=title, trimmed_title=trim(title, MAX_TITLE_LENGTH), authors=authors, journal=journal, year=year,
-                  topic=topic, doi=doi, mesh=mesh, keywords=keywords, url=url_prefix + pid, source=source,
+    result = dict(title=title,
+                  trimmed_title=trim(title, MAX_TITLE_LENGTH),
+                  authors=authors,
+                  journal=journal,
+                  year=year,
+                  doi=doi,
+                  mesh=mesh,
+                  url=url_prefix + pid, source=source,
+                  topic=topic,
+                  keywords=keywords,
                   n_papers=len(analyzer.df),
+                  topics_palette=topics_palette(analyzer.df),
+                  topic_tags=topic_tags,
                   citation_dynamics=components_list(Plotter._plot_paper_citations_per_year(analyzer.df, str(pid))))
 
     if similar_papers:
         result['similar_papers'] = [(pid, trim(title, MAX_TITLE_LENGTH), url_prefix + pid,
-                                     year, cited, f'{similarity:.3f}')
-                                    for pid, title, year, cited, similarity in similar_papers]
-
-    if related_topics:
-        result['related_topics'] = related_topics
+                                     year, cited, f'{similarity:.3f}', topic)
+                                    for pid, title, year, cited, similarity, topic in similar_papers]
 
     abstract = sel['abstract'].values[0]
     if abstract != '':
         result['abstract'] = abstract
 
     if len(prior_papers) > 0:
-        result['prior_papers'] = [(pid, trim(title, MAX_TITLE_LENGTH), url_prefix + pid, year, cited)
-                                  for pid, title, year, cited in prior_papers]
+        result['prior_papers'] = [(pid, trim(title, MAX_TITLE_LENGTH), url_prefix + pid, year, cited, topic + 1)
+                                  for pid, title, year, cited, topic in prior_papers]
 
     if len(derivative_papers) > 0:
-        result['derivative_papers'] = [(pid, trim(title, MAX_TITLE_LENGTH), url_prefix + pid, year, cited)
-                                       for pid, title, year, cited in derivative_papers]
+        result['derivative_papers'] = [(pid, trim(title, MAX_TITLE_LENGTH), url_prefix + pid, year, cited, topic + 1)
+                                       for pid, title, year, cited, topic in derivative_papers]
 
     logger.debug('Papers graph')
     if len(analyzer.df) > 1:
