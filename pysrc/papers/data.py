Fixed word clouds for each topic, moved topic words computation out of analysis

pysrc/papers/data.py
@@ -12,8 +12,8 @@ class AnalysisData:
                  df, cit_df, cocit_grouped_df, bibliographic_coupling_df,
                  top_cited_df, max_gain_df, max_rel_gain_df,
                  corpus, corpus_tokens, corpus_counts,
-                 dendrogram, topics_description,
                  papers_graph, papers_embeddings,
+                 dendrogram,
                  author_stats, journal_stats, numbers_df):
         self.search_query = search_query  # Initial query for analysis
         self.search_ids = search_ids  # Initial ids for analysis
@@ -30,10 +30,9 @@ class AnalysisData:
         self.corpus = corpus
         self.corpus_tokens = corpus_tokens
         self.corpus_counts = corpus_counts
-        self.dendrogram = dendrogram
-        self.topics_description = topics_description
         self.papers_graph = papers_graph
         self.papers_embeddings = papers_embeddings
+        self.dendrogram = dendrogram
         self.author_stats = author_stats
         self.journal_stats = journal_stats
         self.numbers_df = numbers_df
@@ -62,7 +61,6 @@ class AnalysisData:
             max_gain_df=self.max_gain_df.to_json(),
             max_rel_gain_df=self.max_rel_gain_df.to_json(),
             dendrogram=self.dendrogram.tolist() if self.dendrogram is not None else None,
-            topics_description=self.topics_description,
             corpus=self.corpus,
             corpus_tokens=self.corpus_tokens,
             corpus_counts=csm_json,
@@ -118,18 +116,18 @@ class AnalysisData:
         corpus_tokens = fields['corpus_tokens']
         corpus_counts = json.loads(fields['corpus_counts'])
         corpus_counts = csr_matrix((corpus_counts['data'], (corpus_counts['indices'], corpus_counts['indptr'])))
-        # Restore dendrogram
-        dendrogram = fields['dendrogram']
-        if dendrogram is not None:
-            dendrogram = np.array(dendrogram)
-        # Restore topic descriptions
-        topics_description = {int(c): ks for c, ks in fields['topics_description'].items()}  # Restore int components
+
 
         # Restore citation and structure graphs
         papers_graph = json_graph.node_link_graph(fields['papers_graph'])
 
         # Restore original embeddings
         papers_embeddings = np.array(fields['papers_embeddings'])
+
+        # Restore dendrogram
+        dendrogram = fields['dendrogram']
+        if dendrogram is not None:
+            dendrogram = np.array(dendrogram)
 
         # Restore additional analysis
         author_stats = pd.read_json(StringIO(fields['author_stats'])) if fields['author_stats'] is not None else None
@@ -152,10 +150,9 @@ class AnalysisData:
             corpus=corpus,
             corpus_tokens=corpus_tokens,
             corpus_counts=corpus_counts,
-            dendrogram=dendrogram,
-            topics_description=topics_description,
             papers_embeddings=papers_embeddings,
             papers_graph=papers_graph,
+            dendrogram=dendrogram,
             author_stats=author_stats,
             journal_stats=journal_stats,
             numbers_df=numbers_df,