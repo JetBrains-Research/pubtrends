4a74304d1fcc20835c1de4eed2569b8d9a35b9fa
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-05-27 22:42:02 +0200

    Fix nlp packages downloading

--- nlp.sh
+++ nlp.sh
@@ -1,5 +1,5 @@
 # Download all the nltk and spacy resources
 source activate pubtrends \
-    && python -m nltk.downloader averaged_perceptron_tagger \
+    && python -m nltk.downloader punkt_tab averaged_perceptron_tagger averaged_perceptron_tagger_eng \
            punkt stopwords wordnet omw-1.4 \
            && python -m spacy download en_core_web_sm
