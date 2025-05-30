7a452994ef975f9266555d60fefa4135c7596345
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-05-30 22:51:37 +0200

    Fix nltk data caching

--- init.sh
+++ init.sh
@@ -5,9 +5,9 @@
 mkdir -p ~/.pubtrends
 mkdir -p ~/.pubtrends/logs
 mkdir -p ~/.pubtrends/database
-mkdir -p ~/.pubtrends/.cache
 mkdir -p ~/.pubtrends/results
 mkdir -p ~/.pubtrends/nltk_data
 mkdir -p ~/.pubtrends/gensim-data
 mkdir -p ~/.pubtrends/fasttext
 mkdir -p ~/.pubtrends/sentence-transformers
+mkdir -p ~/.pubtrends/faiss
--- nlp.sh
+++ nlp.sh
@@ -1,5 +1,3 @@
-# Download all the nltk and spacy resources
-source activate pubtrends \
-    && python -m nltk.downloader punkt_tab averaged_perceptron_tagger averaged_perceptron_tagger_eng \
-           punkt stopwords wordnet omw-1.4 \
-           && python -m spacy download en_core_web_sm
+# Download all the nltk resources
+python -m nltk.downloader -d /home/user/nltk_data \
+      punkt_tab averaged_perceptron_tagger averaged_perceptron_tagger_eng punkt stopwords wordnet omw-1.4
